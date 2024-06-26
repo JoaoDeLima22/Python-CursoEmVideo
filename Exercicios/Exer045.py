20e78e8dbe8c9e19548365f012a7589e14494e3d
João Victor <joaovictor2010lima@gmail.com>
2024-06-26 00:14:07 -0300

    Exer045

--- Exercicios/Exer045.py
+++ Exercicios/Exer045.py
@@ -10,6 +10,8 @@ As regras são as seguintes:
 - Tesoura vence Papel e perde para Pedra
 '''
 from random import randint
+from time import sleep
+
 print('Selecione uma opção: \n1 - pedra\n2 - papel\n3 - tesoura')
 opcao = int(input("Qual sua opcao? "))
 maquina = randint(1, 3)
@@ -22,13 +24,19 @@ elif opcao == 3:
 else:
     print('selecione uma opção valida')
     exit()
+
+print("JO")
+sleep(0.50)
+print("KEN")
+sleep(0.5)
+print("PÔ!!!")
 if maquina == 1:
     n2 = 'pedra'
 elif maquina == 2:
     n2 = 'papel'
 elif maquina == 3:
     n2 = 'tesoura'
-print('Você escolheu {} e a maquina escolheu {}'.format(n1, n2))
+print('Você escolheu {}  \nA maquina escolheu {}'.format(n1, n2))
 if n1 == n2:
     print('Empate')
 elif n1 =='pedra' and n2=='papel' or n1 =='papel' and n2=='tesoura' or n1 =='tesoura' and n2=='pedra' :
